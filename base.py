680e4d565a6db63365a31025648275798a323f05
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-10-31 00:24:13 +0330

    (REFACTOR) Make code work in multile nimbra monitoring

--- base.py
+++ base.py
@@ -8,20 +8,8 @@ import time
 
 # Input data:
 chrome_driver_path = "./driver/chromedriver.exe"
-nimbra_name        = 'Ghaen'
-username           = "Reza Tanakizadeh"
-password           = "alaki"
 today_date         = '10 Sep 2022'
 yesterday_date     = '5 Oct 2021'
-
-# Urls
-login_url    = "F://Nimbra360_scrapping/temp/login.html"
-alarm_url    = "F://Nimbra360_scrapping/temp/status_alarm.html"
-equ_url      = "F://Nimbra360_scrapping/temp/status_eqm.html"
-dtm_url      = "F://Nimbra360_scrapping/temp/dtm_interface.html"
-pm_trunk_url = "F://Nimbra360_scrapping/temp/pm_overview.html"
-pm_ts_url    = "F://Nimbra360_scrapping/temp/pm_details.html"
-
 
 
 # Make an instance of Chrome driver
@@ -30,54 +18,69 @@ op.add_argument('headless')
 service = Service(executable_path= chrome_driver_path)
 driver  = webdriver.Chrome(service=service, options=op)
 
-
-print(f'\n ############## Start scrapping: {nimbra_name} ##############')
-
-# ############# LOGIN ###############
-login_ins =  login.Login(driver, login_url)
-login_ins.start()
-
-# ############# Alarm ###############
-alarm_ins    =  status_alarm.StatusAlarm(driver, alarm_url, date=today_date)
-alarm_result = alarm_ins.start()
+# Nimbra variable that save informations
+nimbras_info = {
+    'Ghaen' : {'ip': 'F://Nimbra360_scrapping/temp', 'username':'root', 'password':'xxxx'},
+}
 
 
-# ############# Temprature ###############
-eqm_ins    =  status_eqm.StatusEqm(driver, equ_url)
-eqm_result = eqm_ins.start()
+# Iterate on each nimbra 
+for nimbra_name in nimbras_info:
+    nimbra = nimbras_info[nimbra_name]
+
+    print('\n')
+    print(f"**************** {nimbra_name} ****************")
+
+    # Urls
+    login_url    = f"{nimbra['ip']}/login.html"
+    alarm_url    = f"{nimbra['ip']}/status_alarm.html"
+    equ_url      = f"{nimbra['ip']}/status_eqm.html"
+    dtm_url      = f"{nimbra['ip']}/dtm_interface.html"
+    pm_trunk_url = f"{nimbra['ip']}/pm_overview.html?tif"
+    pm_ts_url    = f"{nimbra['ip']}/pm_details.html"
 
 
-# ############# DTM ###############
-dtm_ins    =  dtm_interface.DTMInterface(driver, dtm_url)
-dtm_result = dtm_ins.start()
+    ## Login
+    login_ins =  login.Login(driver, login_url, nimbra['username'], nimbra['password'])
+    login_ins.start()
+
+    ## Alarm
+    alarm_ins    =  status_alarm.StatusAlarm(driver, alarm_url, date=today_date)
+    alarm_result = alarm_ins.start()
+
+    ## Temprature 
+    eqm_ins    =  status_eqm.StatusEqm(driver, equ_url)
+    eqm_result = eqm_ins.start()
+
+    # DTM 
+    dtm_ins    =  dtm_interface.DTMInterface(driver, dtm_url)
+    dtm_result = dtm_ins.start()
 
 
-# ############# TRUNK ###############
-trunk_ins    =  pm_overview.PMOverview(driver, pm_trunk_url)
-trunk_result = trunk_ins.start()
+    ## TRUNK
+    trunk_ins    =  pm_overview.PMOverview(driver, pm_trunk_url)
+    trunk_result = trunk_ins.start()
 
 
-############ TS1 ################
-ts1_ins    =  pm_details.PMDetails(driver, pm_ts_url, 1,yesterday_date)
-ts1_result = ts1_ins.start()
+    ## TS1 
+    ts1_ins    =  pm_details.PMDetails(driver, pm_ts_url, 0, yesterday_date)
+    ts1_result = ts1_ins.start()
 
 
-############ TS2 ################
-ts2_ins    =  pm_details.PMDetails(driver, pm_ts_url, 2,'5 Oct 2021')
-ts2_result = ts2_ins.start()
+    ## TS2 
+    ts2_ins    =  pm_details.PMDetails(driver, pm_ts_url, 1, yesterday_date)
+    ts2_result = ts2_ins.start()
+
+        # Print Results
+    print('\n')
+    print("+ Temprature:  ", eqm_result)
+    alarm_ins.sp_print(alarm_result)
+    dtm_ins.sp_print(dtm_result)
+    trunk_ins.sp_print(trunk_result)
+    ts1_ins.sp_print(ts1_result)
+    ts2_ins.sp_print(ts2_result)
 
 
 
 driver.close()
-
-# Print Results
-print('\n')
-print("+ Temprature:  ", eqm_result)
-alarm_ins.sp_print(alarm_result)
-dtm_ins.sp_print(dtm_result)
-trunk_ins.sp_print(trunk_result)
-ts1_ins.sp_print(ts1_result)
-ts2_ins.sp_print(ts2_result)
-
-
 exit()
