8e0af7bc7840f1ef0df7ce12a6959bab5bc0f669
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-11-11 13:57:53 +0330

    (REFACTOR) Make a class for totalscrapping

--- base.py
+++ base.py
@@ -5,82 +5,80 @@ from data_pages import login, status_alarm, status_eqm, dtm_interface,pm_overvie
 import time
 
 
+# Make base class
+class Base():
+    def __init__(self, chrome_driver_path = "./driver/chromedriver.exe") -> None:
+        self.chrome_driver_path = chrome_driver_path
 
-# Input data:
-chrome_driver_path = "./driver/chromedriver.exe"
-today_date         = '10 Sep 2022'
-yesterday_date     = '5 Oct 2021'
-
-
-# Make an instance of Chrome driver
-op = webdriver.ChromeOptions()
-op.add_argument('headless')
-service = Service(executable_path= chrome_driver_path)
-driver  = webdriver.Chrome(service=service, options=op)
-
-# Nimbra variable that save informations
-nimbras_info = {
-    'Ghaen' : {'ip': 'F://Nimbra360_scrapping/temp', 'username':'root', 'password':'xxxx'},
-}
-
-
-# Iterate on each nimbra 
-for nimbra_name in nimbras_info:
-    nimbra = nimbras_info[nimbra_name]
+        # Make an instance of Chrome driver
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        service = Service(executable_path= chrome_driver_path)
+        self.driver  = webdriver.Chrome(service=service, options=op)
     
-    print('\n')
-    print(f"**************** {nimbra_name} ****************")
+        # Nimbra variable that save informations
+        self.nimbras_info = {
+            'Ghaen' : {'ip': 'F://Nimbra360_scrapping/temp', 'username':'root', 'password':'xxxx'},
+        }
+    
+    # Start Scrapping
+    def start(self, today_date='10 Sep 2022', yesterday_date='5 Oct 2021',stations=['Ghaen']):
+        # Iterate on each nimbra 
+        for nimbra_name in stations:
+            nimbra = self.nimbras_info[nimbra_name]
+            
+            print('\n')
+            print(f"**************** {nimbra_name} ****************")
 
-    # Urls
-    login_url    = f"{nimbra['ip']}/login.html"
-    alarm_url    = f"{nimbra['ip']}/status_alarm.html"
-    equ_url      = f"{nimbra['ip']}/status_eqm.html"
-    dtm_url      = f"{nimbra['ip']}/dtm_interface.html"
-    pm_trunk_url = f"{nimbra['ip']}/pm_overview.html?tif"
-    pm_ts_url    = f"{nimbra['ip']}/pm_details.html"
-
-
-    ## Login
-    login_ins =  login.Login(driver, login_url, nimbra['username'], nimbra['password'], login_name=nimbra_name)
-    login_ins.start()
-
-    ## Alarm
-    alarm_ins    =  status_alarm.StatusAlarm(driver, alarm_url, date=today_date)
-    alarm_result = alarm_ins.start()
-
-    ## Temprature 
-    eqm_ins    =  status_eqm.StatusEqm(driver, equ_url)
-    eqm_result = eqm_ins.start()
-
-    # DTM 
-    dtm_ins    =  dtm_interface.DTMInterface(driver, dtm_url)
-    dtm_result = dtm_ins.start()
+            # Urls
+            login_url    = f"{nimbra['ip']}/login.html"
+            alarm_url    = f"{nimbra['ip']}/status_alarm.html"
+            equ_url      = f"{nimbra['ip']}/status_eqm.html"
+            dtm_url      = f"{nimbra['ip']}/dtm_interface.html"
+            pm_trunk_url = f"{nimbra['ip']}/pm_overview.html?tif"
+            pm_ts_url    = f"{nimbra['ip']}/pm_details.html"
 
 
-    ## TRUNK
-    trunk_ins    =  pm_overview.PMOverview(driver, pm_trunk_url)
-    trunk_result = trunk_ins.start()
+            ## Login
+            login_ins =  login.Login(self.driver, login_url, nimbra['username'], nimbra['password'], login_name=nimbra_name)
+            login_ins.start()
+
+            ## Alarm
+            alarm_ins    =  status_alarm.StatusAlarm(self.driver, alarm_url, date=today_date)
+            alarm_result = alarm_ins.start()
+
+            ## Temprature 
+            eqm_ins    =  status_eqm.StatusEqm(self.driver, equ_url)
+            eqm_result = eqm_ins.start()
+
+            # DTM 
+            dtm_ins    =  dtm_interface.DTMInterface(self.driver, dtm_url)
+            dtm_result = dtm_ins.start()
 
 
-    ## TS1 
-    ts1_ins    =  pm_details.PMDetails(driver, pm_ts_url, 0, yesterday_date)
-    ts1_result = ts1_ins.start()
+            ## TRUNK
+            trunk_ins    =  pm_overview.PMOverview(self.driver, pm_trunk_url)
+            trunk_result = trunk_ins.start()
 
 
-    ## TS2 
-    ts2_ins    =  pm_details.PMDetails(driver, pm_ts_url, 1, yesterday_date)
-    ts2_result = ts2_ins.start()
-
-        # Print Results
-    print('\n')
-    print("+ Temprature:  ", eqm_result)
-    alarm_ins.sp_print(alarm_result)
-    dtm_ins.sp_print(dtm_result)
-    trunk_ins.sp_print(trunk_result)
-    ts1_ins.sp_print(ts1_result)
-    ts2_ins.sp_print(ts2_result)
+            ## TS1 
+            ts1_ins    =  pm_details.PMDetails(self.driver, pm_ts_url, 0, yesterday_date)
+            ts1_result = ts1_ins.start()
 
 
+            ## TS2 
+            ts2_ins    =  pm_details.PMDetails(self.driver, pm_ts_url, 1, yesterday_date)
+            ts2_result = ts2_ins.start()
 
-driver.close()
-exit()
+            # Print Results
+            print('\n')
+            print("+ Temprature:  ", eqm_result)
+            alarm_ins.sp_print(alarm_result)
+            dtm_ins.sp_print(dtm_result)
+            trunk_ins.sp_print(trunk_result)
+            ts1_ins.sp_print(ts1_result)
+            ts2_ins.sp_print(ts2_result)
+
+            
+
+
